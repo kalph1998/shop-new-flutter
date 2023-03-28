b6cf24ea2bc752e699d535d110e3053b7e307af4
Kalph upadhyay <kalph.upadhyay@intimetec.com>
2023-03-28 18:59:02 +0530

    refactoer

--- lib/widgets/home_app_bar.dart
+++ lib/widgets/home_app_bar.dart
@@ -9,44 +9,47 @@ class HomeAppBar extends StatelessWidget {
     return Container(
       color: Colors.white,
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Row(
-          children: const [
-            Icon(
-              Icons.sort,
-              size: 30,
-              color: Color(0xFF4C53A5),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              'Shop',
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.bold,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: const [
+              Icon(
+                Icons.sort,
+                size: 30,
                 color: Color(0xFF4C53A5),
               ),
-            )
-          ],
-        ),
-        badges.Badge(
-          badgeContent: const Text(
-            '3',
-            style: TextStyle(color: Colors.white),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                'Shop',
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4C53A5),
+                ),
+              )
+            ],
           ),
-          badgeStyle: const badges.BadgeStyle(
-              badgeColor: Colors.red, padding: EdgeInsets.all(6)),
-          child: InkWell(
-            child: const Icon(
-              Icons.shopping_bag_outlined,
-              size: 30,
-              color: Color(0xFF4C53A5),
+          badges.Badge(
+            badgeContent: const Text(
+              '3',
+              style: TextStyle(color: Colors.white),
             ),
-            onTap: () {},
+            badgeStyle: const badges.BadgeStyle(
+                badgeColor: Colors.red, padding: EdgeInsets.all(6)),
+            child: InkWell(
+              child: const Icon(
+                Icons.shopping_bag_outlined,
+                size: 30,
+                color: Color(0xFF4C53A5),
+              ),
+              onTap: () {},
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
